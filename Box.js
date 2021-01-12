2bbe5bf514d4cebd064a1df260731c7b32020862
MillerZacharySA <71454397+MillerZacharySA@users.noreply.github.com>
2021-01-11 18:32:46 -0800

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,25 @@
+class Box{
+    constructor(x, y, width, height, angle) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+
+        this.x = x;
+        this.y = y;
+
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+    display(color){
+        var pos =this.body.position;
+        push();
+        rectMode(CENTER);
+        fill(color);
+        rect(pos.x, pos.y, this.width, this.height);
+        pop();
+    }
+}
